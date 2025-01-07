e608bdfd99d1934e9adc1f8589571577d0f630df
tomoyan99 <moyant0909sp@gmail.com>
2025-01-07 20:13:44 +0900

    [main] sideBarを作成

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,33 +1,30 @@
 import React from "react";
-import {Box,Grid, GridItem} from "@chakra-ui/react"
+import {Box,Grid, GridItem,Text} from "@chakra-ui/react"
 // import {MyDrawer} from "./components/MyDrawer";
 import SideBar from "./components/SideBar";
+import Typography from "@mui/material/Typography";
 // import SideBar from "./components/SideBar2";
 
 function App(): React.ReactElement {
   return (
-      <Grid
-          h="99vh"
-          templateRows="repeat(4, 1fr)"
-          templateColumns="repeat(5, 1fr)"
-          gap={3}
-      >
-        <GridItem rowSpan={4} colSpan={1}>
-          <Box bg={"gray"} h={"100%"}>
-            <SideBar/>
-            rowSpan=2
-          </Box>
-        </GridItem>
-        <GridItem rowSpan={1} colSpan={4}>
-          <Box bg={"gray"} h={"100%"}>colSpan=4</Box>
-        </GridItem>
-        <GridItem rowSpan={3} colSpan={2}>
-          <Box bg={"gray"} h={"100%"}>colSpan=2</Box>
-        </GridItem>
-        <GridItem rowSpan={3} colSpan={2}>
-          <Box bg={"gray"} h={"100%"}>colSpan=2</Box>
-        </GridItem>
-      </Grid>
+      <SideBar>
+        <Grid
+            h="100%"
+            templateRows="repeat(4, 1fr)"
+            templateColumns="repeat(4, 1fr)"
+            gap={3}
+        >
+          <GridItem rowSpan={1} colSpan={4} bg={"brown"}>
+            <Text>タスク</Text>
+          </GridItem>
+          <GridItem rowSpan={3} colSpan={2} bg={"brown"}>
+            <Text>履歴</Text>
+          </GridItem>
+          <GridItem rowSpan={3} colSpan={2} bg={"brown"}>
+            <Text>トピック・ニュース</Text>
+          </GridItem>
+        </Grid>
+      </SideBar>
   );
 }
 
