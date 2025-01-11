3c339fab74e79d22a3415be3b1f02fcaadd96e40
tomoyan99 <moyant0909sp@gmail.com>
2025-01-12 00:44:27 +0900

    [stopwatch_draft] とジル

--- src/renderer/src/components/Dialogs/StopWatch.tsx
+++ src/renderer/src/components/Dialogs/StopWatch.tsx
@@ -2,19 +2,17 @@ import MyDialog from "../MyDialog";
 import { Box, Button } from "@chakra-ui/react";
 import * as React from "react";
 import { useStopwatchContext } from "../../context/StopwatchContext";
+import { DialogActionTrigger } from "../../ui/dialog";
 
 export function StopWatch(props:{children?:React.ReactNode,task?:string}) {
     const { startStopwatch, stopStopwatch } = useStopwatchContext();
     const [comment, setComment] = React.useState("");
-    const [dialogOpen, setDialogOpen] = React.useState(false);
 
   // ダイアログを開始するトリガーにするためのステート
   // -> MyDialog.Root の open/close を外部で制御するなら props でもOK
 
     return(
         <MyDialog.Root 
-        open={dialogOpen}
-        onOpenChange={setDialogOpen}
         closeOnInteractOutside={false}>
             <MyDialog.Trigger>
                 <Box>
@@ -37,16 +35,16 @@ export function StopWatch(props:{children?:React.ReactNode,task?:string}) {
                 </MyDialog.Body>
                 <MyDialog.Footer>
         {/* 「開始」ボタン：ストップウォッチを開始してダイアログを閉じる */}
-         <Button
-           colorScheme="orange"
-           onClick={() => {
-             startStopwatch();
-             // ここでダイアログを閉じる
-             setDialogOpen(false);
-           }}
-         >
-           開始
-         </Button>
+        <DialogActionTrigger asChild>
+            <Button
+                colorScheme="orange"
+                onClick={() => {
+                    startStopwatch();
+                }}
+            >
+                開始
+            </Button>
+        </DialogActionTrigger>
                 </MyDialog.Footer>
             </MyDialog.Content>
         </MyDialog.Root>
