056e4ca7a2bd01804757366bcc463f6b4190c7f5
NEWACTIONLEGEND <T122015@ed.sus.ac.jp>
2025-01-13 20:59:31 +0900

    o1の追加

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -3,6 +3,17 @@ import { ElectronAPI } from '@electron-toolkit/preload'
 export interface API {
   fetchUsers: () => Promise<{ id: number; name: string; age: number }[]>;
   addUser: (user: { name: string; age: number }) => Promise<void>;
+
+  // ★ 追加: IPC呼び出しメソッド
+  postTweet: (message: string) => Promise<any>;
+  getHistory: () => Promise<any[]>;
+  startSession: (taskName: string) => Promise<number>;
+  stopSession: (sessionId: number, note: string) => Promise<boolean>;
+  getSummary: () => Promise<{
+    tweets: any[];
+    sessions: any[];
+  }>;
+  markSummarized: (tweetIds: number[], sessionIds: number[]) => Promise<boolean>;
 }
 
 declare global {
